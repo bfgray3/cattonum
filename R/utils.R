e1cf9def4f15424f6e8a49546351aecec8506b85
Bernie Gray <bfgray3@gmail.com>
2017-12-09 16:23:58 -0500

    improve utility functions

--- R/utils.R
+++ R/utils.R
@@ -1,10 +1,38 @@
+####################
+### colname_list ###
+####################
+
+colname_list <- function(.nms) {
+  paste(sQuote(.nms), collapse = ", ")
+}
+
 #################
 ### pick_cols ###
 #################
 
 pick_cols <- function(.df, ...) {
-  if (length(substitute(alist(...))) == 1L) all_cats(.df)
-  else tidyselect::vars_select(names(.df), ...)
+  if (length(substitute(alist(...))) == 1L) {
+    all_cats(.df)
+  } else {
+    nms <- names(.df)
+    extra_cols <- nonexistent_cols(nms, ...)
+    if (! length(extra_cols)) {
+      tidyselect::vars_select(nms, ...)
+    } else {
+      plural <- length(extra_cols) > 1L
+      stop("Cannot encode column", if (plural) "s", " ",
+           colname_list(extra_cols), ".", call. = FALSE)
+    }
+  }
+}
+
+########################
+### nonexistent_cols ###
+########################
+
+nonexistent_cols <- function(.nms, ...) {
+  supplied <- vapply(eval(substitute(alist(...))), as.character, character(1L))
+  setdiff(supplied, .nms)
 }
 
 ################
