fbcf9353c57de9e0fb912f5fb0036d4ad78717fb
Bernie Gray <bfgray3@gmail.com>
2017-12-10 15:18:47 -0500

    pick_cols can now handle columns specified in different ways

--- R/utils.R
+++ R/utils.R
@@ -7,11 +7,11 @@ mat_or_df <- function(.df) {
   if (all_numerics) as.matrix(.df) else .df
 }
 
-#####################
-### validate_cols ###
-#####################
+##########################
+### validate_col_types ###
+##########################
 
-validate_cols <- function(.df) {
+validate_col_types <- function(.df) {
   good_cols <- vapply(.df, good_col_type, logical(1L))
   if (! all(good_cols)) {
     bad_cols <- names(df)[! good_cols]
@@ -36,6 +36,14 @@ colname_list <- function(.nms) {
   paste(paste0("'", .nms, "'"), collapse = ", ")
 }
 
+####################
+### dots_to_char ###
+####################
+
+dots_to_char <- function(...) {
+  deparse(substitute(...))
+}
+
 #################
 ### pick_cols ###
 #################
@@ -45,27 +53,15 @@ pick_cols <- function(.df, ...) {
     all_cats(.df)
   } else {
     nms <- names(.df)
-    extra_cols <- nonexistent_cols(nms, ...)
-    if (! length(extra_cols)) {
-      tidyselect::vars_select(nms, ...)
-    } else {
-      plural <- length(extra_cols) > 1L
-      stop("Cannot encode column", if (plural) "s", " ",
-           colname_list(extra_cols), ".", call. = FALSE)
+    col_spec <- dots_to_char(...)
+    df_name <- deparse(substitute(.df))
+    bad_cols_error <- function(e) {
+      stop(col_spec, " is not a valid column specification for ",
+           df_name, ".", call. = FALSE)
     }
+    tryCatch(tidyselect::vars_select(nms, ...),
+             error = bad_cols_error)
   }
-}
-
-########################
-### nonexistent_cols ###
-########################
-
-nonexistent_cols <- function(.nms, ...) {
-  supplied_symbols <- eval(substitute(alist(...)))
-  supplied_nms <- vapply(supplied_symbols,
-                         as.character,
-                         character(1L))
-  setdiff(supplied_nms, .nms)
 }
 
 ################
