78859ae4d6a7e0431f8d2a595b64c8299794d2c8
Bernie Gray <bfgray3@gmail.com>
2017-12-10 19:11:53 -0500

    fix catto_freq tests

--- tests/testthat/test_freq.R
+++ tests/testthat/test_freq.R
@@ -49,11 +49,10 @@ test_that("catto_freq works.", {
   ### SUBSET OF CATEGORICAL COLUMNS ###
 
   expected_x1_only <- data.frame(y, x1 = c(2, 2, NA, 2, 2), x2)
-  char_and_bare <- list(catto_freq(df_fact, "x1", response = "y"),
-                        catto_freq(df_fact, x1, response = y),
+  char_and_bare <- list(catto_freq(df_fact, "x1"),
+                        catto_freq(df_fact, x1),
                         catto_freq(df_fact,
-                                   tidyselect::one_of("x1"),
-                                   response = "y"))
+                                   tidyselect::one_of("x1")))
 
   for (result in char_and_bare) {
 
@@ -61,13 +60,6 @@ test_that("catto_freq works.", {
     expect_equal(result, expected_x1_only)
 
   }
-
-  ### RESPONSE NOT SPECIFIED ###
-
-  expect_message(no_response <- catto_freq(df_fact, x1),
-                 paste("`response` not supplied; using first column",
-                       "'y' as the response variable."))
-  expect_equal(no_response, expected_x1_only)
 
 })
 
