ebd31f395a34c1e33ed046e22ec90e8c60b68dac
Bernie Gray <bfgray3@gmail.com>
2017-12-10 19:10:00 -0500

    remove response argument from catto_freq

--- R/mean_freq.R
+++ R/mean_freq.R
@@ -8,51 +8,59 @@ ave_na <- function(.grouping, .x, .f) {
   replace(summarized, nas, NA)
 }
 
-##########################
-### encode_no_new_cols ###
-##########################
-
-encode_no_new_cols <- function(.fun = function(...) mean(..., na.rm = TRUE)) {
-
-  function(dataframe, ..., response, verbose = TRUE) {
-
-    validate_col_types(dataframe)
-
-    nms <- names(dataframe)
-
-    if (missing(response)) {
-      response <- nms[1L]
-      if (verbose) {
-        message("`response` not supplied; using first column '",
-                response, "' as the response variable.")
-      }
-    } else {
-      subs_resp <- substitute(response)
-      response <- if (is.name(subs_resp)) deparse(subs_resp) else subs_resp
-    }
-
-    cats <- pick_cols(dataframe, ...)
-    dataframe[cats] <- lapply(dataframe[cats],
-                              ave_na,
-                              .x = dataframe[[response]],
-                              .f = .fun)
-
-    mat_or_df(dataframe)
-
-  }
-
-}
-
 ##################
 ### catto_mean ###
 ##################
 
-catto_mean <- encode_no_new_cols()
+catto_mean <- function(dataframe,
+                       ...,
+                       response,
+                       rm_na = TRUE,
+                       verbose = TRUE) {
+
+  validate_col_types(dataframe)
+
+  nms <- names(dataframe)
+
+  if (missing(response)) {
+    response <- nms[1L]
+    if (verbose) {
+      message("`response` not supplied; using first column '",
+              response, "' as the response variable.")
+    }
+  } else {
+    subs_resp <- substitute(response)
+    response <- if (is.name(subs_resp)) deparse(subs_resp) else subs_resp
+  }
+
+  cats <- pick_cols(dataframe, ...)
+  dataframe[cats] <- lapply(dataframe[cats],
+                            ave_na,
+                            .x = dataframe[[response]],
+                            .f = function(...) mean(..., na.rm = rm_na))
+
+  mat_or_df(dataframe)
+
+}
 
 ##################
 ### catto_freq ###
 ##################
 
-catto_freq <- encode_no_new_cols(.fun = length)
+catto_freq <-  function(dataframe, ..., verbose = TRUE) {
+
+  validate_col_types(dataframe)
+
+  nms <- names(dataframe)
+
+  cats <- pick_cols(dataframe, ...)
+  dataframe[cats] <- lapply(dataframe[cats],
+                            ave_na,
+                            .x = dataframe[[1L]],
+                            .f = length)
+
+  mat_or_df(dataframe)
+
+}
 
 ###
