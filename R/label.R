76da3788a40a9a4cc91ec92ab138d28e31b28075
Bernie Gray <bfgray3@gmail.com>
2017-12-11 19:57:31 -0500

    add different ways to apply labels; needs to be cleaned up

--- R/label.R
+++ R/label.R
@@ -1,11 +1,45 @@
-###################
-### make_labels ###
-###################
+# TODO: add support for reverse ordered, random, observed ordered labels
 
-make_labels <- function(.x) {
+######################
+### ordered_labels ###
+######################
+
+ordered_labels <- function(.decreasing) {
+
+  function(.x) {
+
+    if (is.factor(.x)) .x <- as.character(.x)
+    ordered_labs <- ordered_by_freq(.x, .decreasing)
+    lkp <- data.frame(new_lab = seq_along(ordered_labs),
+                      row.names = ordered_labs)
+    lkp[.x, ]
+
+  }
+
+}
+
+#####################
+### random_labels ###
+#####################
+
+random_labels <- function(.x, seed = 4444) {
 
   if (is.factor(.x)) .x <- as.character(.x)
-  ordered_labs <- ordered_by_freq(.x)
+  ordered_labs <- sample(.x)
+  lkp <- data.frame(new_lab = seq_along(ordered_labs),
+                    row.names = ordered_labs)
+  lkp[.x, ]
+
+}
+
+#########################
+### appearance_labels ###
+#########################
+
+random_labels <- function(.x) {
+
+  if (is.factor(.x)) .x <- as.character(.x)
+  ordered_labs <- unique(.x)
   lkp <- data.frame(new_lab = seq_along(ordered_labs),
                     row.names = ordered_labs)
   lkp[.x, ]
@@ -13,17 +47,43 @@ make_labels <- function(.x) {
 }
 
 ###################
+### incr_labels ###
+###################
+
+incr_labels <- ordered_labels(FALSE)
+
+###################
+### decr_labels ###
+###################
+
+decr_labels <- ordered_labels(TRUE)
+
+###################
 ### catto_label ###
 ###################
 
-catto_label <- function(dataframe, ..., response, verbose = TRUE) {
+catto_label <- function(dataframe,
+                        ...,
+                        ordering = c("increasing",
+                                     "decreasing",
+                                     "observed",
+                                     "random"),
+                        verbose = TRUE) {
 
   validate_col_types(dataframe)
 
   nms <- names(dataframe)
 
+  ordering <- match.arg(ordering)
+
+  label_maker <- switch(ordering,
+                        increasing = incr_labels,
+                        decreasing = decr_labels,
+                        observed = appearance_labels,
+                        random = random_labels)
+
   cats <- pick_cols(dataframe, ...)
-  dataframe[cats] <- lapply(dataframe[cats], make_labels)
+  dataframe[cats] <- lapply(dataframe[cats], label_maker)
 
   mat_or_df(dataframe)
 
