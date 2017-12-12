c56c79622ce668e96f2fc6f84d7169942932ebbc
Bernie Gray <bfgray3@gmail.com>
2017-12-11 22:13:40 -0500

    improve test coverage for catto_label

--- tests/testthat/test_label.R
+++ tests/testthat/test_label.R
@@ -11,52 +11,91 @@ x2 <- c("c", "c", "c", "c", "d", "d")
 df_fact <- data.frame(y = y, x1 = x1, x2 = x2)
 df_char <- data.frame(y = y, x1 = x1, x2 = x2, stringsAsFactors = FALSE)
 
+incr_df <- data.frame(y = y,
+                      x1 = c(2, 1, NA, 1, 2, 2),
+                      x2 = c(2, 2, 2, 2, 1, 1))
+
+decr_df <- data.frame(y = y,
+                      x1 = c(1, 2, NA, 2, 1, 1),
+                      x2 = c(1, 1, 1, 1, 2, 2))
+
+order_df <- data.frame(y = y,
+                       x1 = c(1, 2, NA, 2, 1, 1),
+                       x2 = c(1, 1, 1, 1, 2, 2))
+
 ######################
 ### LABEL ENCODING ###
 ######################
 
 test_that("catto_label works.", {
 
-  ### ALL CATEGORICAL COLUMNS ###
+  order_options <- c("increasing",
+                     "decreasing",
+                     "observed",
+                     "random")
 
-  label_fact1 <- catto_label(df_fact)
-  label_fact2 <- catto_label(df_fact, x1, x2)
-  label_fact3 <- catto_label(df_fact, c(x1, x2))
-  label_fact4 <- catto_label(df_fact, c("x1", "x2"))
-  label_fact5 <- catto_label(df_fact, tidyselect::one_of(c("x1", "x2")))
-  label_fact6 <- catto_label(df_fact, tidyselect::one_of("x1", "x2"))
-  label_char1 <- catto_label(df_char)
-  label_char2 <- catto_label(df_char, x1, x2)
-  label_char3 <- catto_label(df_char, c(x1, x2))
-  label_char4 <- catto_label(df_char, c("x1", "x2"))
-  label_char5 <- catto_label(df_char, tidyselect::one_of(c("x1", "x2")))
-  label_char6 <- catto_label(df_char, tidyselect::one_of("x1", "x2"))
+  for (ordering_method in order_options) {
 
-  expected_df_both <- data.frame(y = y,
-                                 x1 = c(2, 1, NA, 1, 2, 2),
-                                 x2 = c(2, 2, 2, 2, 1, 1))
-  expected_both <- as.matrix(expected_df_both)
-  result_names <- c(paste0("label_fact", seq_len(6)), paste0("label_char", seq_len(6)))
-  char_and_fact <- mget(result_names)
+    ### ALL CATEGORICAL COLUMNS ###
 
-  for (m in char_and_fact) {
+    label_fact1 <- catto_label(df_fact,
+                               ordering = ordering_method)
+    label_fact2 <- catto_label(df_fact, x1, x2,
+                               ordering = ordering_method)
+    label_fact3 <- catto_label(df_fact, c(x1, x2),
+                               ordering = ordering_method)
+    label_fact4 <- catto_label(df_fact, c("x1", "x2"),
+                               ordering = ordering_method)
+    label_fact5 <- catto_label(df_fact, tidyselect::one_of(c("x1", "x2")),
+                               ordering = ordering_method)
+    label_fact6 <- catto_label(df_fact, tidyselect::one_of("x1", "x2"),
+                               ordering = ordering_method)
+    label_char1 <- catto_label(df_char,
+                               ordering = ordering_method)
+    label_char2 <- catto_label(df_char, x1, x2,
+                               ordering = ordering_method)
+    label_char3 <- catto_label(df_char, c(x1, x2),
+                               ordering = ordering_method)
+    label_char4 <- catto_label(df_char, c("x1", "x2"),
+                               ordering = ordering_method)
+    label_char5 <- catto_label(df_char, tidyselect::one_of(c("x1", "x2")),
+                               ordering = ordering_method)
+    label_char6 <- catto_label(df_char, tidyselect::one_of("x1", "x2"),
+                               ordering = ordering_method)
 
-    expect_is(m, "matrix")
-    expect_equal(m, expected_both)
+    expected_df_both <- switch(ordering_method,
+                               increasing = incr_df,
+                               decreasing = decr_df,
+                               observed = ,
+                               random = order_df)
 
-  }
+    expected_both <- as.matrix(expected_df_both)
+    result_names <- c(paste0("label_fact", seq_len(6)), paste0("label_char", seq_len(6)))
+    char_and_fact <- mget(result_names)
 
-  ### SUBSET OF CATEGORICAL COLUMNS ###
+    if (ordering_method != "random") {
+      for (m in char_and_fact) {
+        expect_equal(m, expected_both)
+      }
+    } else {
+      for (j in seq_len(ncol(expected_both))) {
+        expect_equal(sort(m[ , 1], na.last = TRUE),
+                     sort(expected_both[ , 1], na.last = TRUE))
+      }
+    }
 
-  expected_x1_only <- data.frame(y = y, x1 = c(2, 1, NA, 1, 2, 2), x2 = x2)
-  char_and_bare <- list(catto_label(df_fact, "x1"),
-                        catto_label(df_fact, x1),
-                        catto_label(df_fact, tidyselect::one_of("x1")))
+    ### SUBSET OF CATEGORICAL COLUMNS ###
 
-  for (result in char_and_bare) {
+    expected_x1_only <- data.frame(y = y, x1 = c(2, 1, NA, 1, 2, 2), x2 = x2)
+    char_and_bare <- list(catto_label(df_fact, "x1"),
+                          catto_label(df_fact, x1),
+                          catto_label(df_fact, tidyselect::one_of("x1")))
 
-    expect_is(result, "data.frame")
-    expect_equal(result, expected_x1_only)
+    for (result in char_and_bare) {
+
+      expect_equal(result, expected_x1_only)
+
+    }
 
   }
 
