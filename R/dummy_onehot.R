8ed6ac9e97a01df5209f89553c2655bb2bb66c5e
Bernie Gray <bfgray3@gmail.com>
2018-01-07 22:05:52 -0500

    enable catto_onehot to handle test data

--- R/dummy_onehot.R
+++ R/dummy_onehot.R
@@ -4,40 +4,55 @@
 
 make_form <- function(.vars) {
   string <- paste("~", paste(.vars, collapse = " + "), "- 1")
-  as.formula(string)
+  stats::as.formula(string)
 }
 
-#################
-### to_factor ###
-#################
+##################
+### set_levels ###
+##################
 
-to_factor <- function(.x) {
-  if (is.factor(.x)) {
-    if (anyNA(.x)) {
-      levels(.x) <- append(levels(.x), "NA")
-      .x[is.na(.x)] <- "NA"
-    }
-    .x
-  } else {
-    .x[is.na(.x)] <- "NA"
-    factor(.x)
-  }
+# TODO: just use `levels<-`
+set_levels <- function(.f, .l) {
+  factor(.f, levels = .l)
 }
 
 ####################
 ### model_matrix ###
 ####################
 
-model_matrix <- function(.df, .cols = names(.df)) {
-  .df[.cols] <- lapply(.df[.cols], to_factor)
+model_matrix <- function(.df, .cols = names(.df), .levels = NULL) {
+  .df[.cols] <- lapply(.df[.cols], as.factor)
+  if (! is.null(.levels)) {
+    .df[.cols] <- Map(set_levels, .f = .df[.cols], .l = .levels)
+  }
   form <- make_form(.cols)
-  mf <- stats::model.frame(form, data = .df)
-  cs <- lapply(.df[.cols], contrasts, contrasts = FALSE)
+  mf <- stats::model.frame(form, data = .df, na.action = na.pass)
+  cs <- lapply(.df[.cols], stats::contrasts, contrasts = FALSE)
   mm <- stats::model.matrix(form, mf, contrasts.arg = cs)
   attr(mm, "contrasts") <- NULL
   attr(mm, "assign") <- NULL
   rownames(mm) <- NULL
   mm
+}
+
+####################
+### na_new_levels ##
+####################
+
+na_new_levels <- function(.x, .orig_levels) {
+  replace(.x, ! is.element(.x, .orig_levels), NA)
+}
+
+####################
+### df_to_binary ###
+####################
+
+df_to_binary <- function(.df, .cats, .levs = NULL) {
+  df_cat <- .df[.cats]
+  df_keep <- .df[setdiff(names(.df), .cats)]
+  rm(.df)
+  df_cat <- model_matrix(df_cat, .levels = .levs)
+  cbind(df_keep, df_cat)
 }
 
 ####################
@@ -52,16 +67,21 @@ catto_onehot <- function(train, ..., test) {
 
   cats <- pick_cols(train, ...)
 
-  df_cat <- train[cats]
-  df_keep <- train[setdiff(names(train), cats)]
+  train_expanded <- df_to_binary(train, cats)
 
-  rm(train)
+  if (test_also) {
+    train_levels <- lapply(train[cats], unique)
+    test[cats] <- Map(na_new_levels,
+                      .x = test[cats],
+                      .orig_levels = train_levels)
+    test_expanded <- df_to_binary(test, cats, .levs = train_levels)
+  }
 
-  df_cat <- model_matrix(df_cat)
-
-  expanded <- cbind(df_keep, df_cat)
-
-  mat_or_df(expanded)
+  if (! test_also) {
+    mat_or_df(train_expanded)
+  } else {
+    lapply(list(train = train_expanded, test = test_expanded), mat_or_df)
+  }
 
 }
 
