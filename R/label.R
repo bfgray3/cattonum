33e599c53833262dd36cf41bedf2fb6a23b53f7d
Bernie Gray <bfgray3@gmail.com>
2019-01-17 21:14:07 -0600

    styler::style_pkg(strict = TRUE)

--- R/label.R
+++ R/label.R
@@ -97,16 +97,16 @@ lkp_from_list <- function(.ord, .orig_col) {
 #'   test datasets.
 #' @examples
 #' catto_label(iris)
-#'
+#' 
 #' y <- 2^(0:5)
 #' x1 <- c("a", "b", NA, "b", "a", "a")
 #' x2 <- c("c", "c", "c", "d", "d", "c")
 #' df_fact <- data.frame(y, x1, x2)
-#'
+#' 
 #' catto_label(df_fact,
 #'   ordering = list(c("b", "a"), c("c", "d"))
 #' )
-#'
+#' 
 #' catto_label(df_fact, ordering = c("increasing", "decreasing"))
 #' @export
 catto_label <- function(train,
