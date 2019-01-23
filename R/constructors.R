09e8219a70104ca144996b0ab8c5304444f9bf45
Bernie Gray <bfgray3@gmail.com>
2019-01-22 21:09:24 -0500

    first pass at cattonum_dfs()

--- R/constructors.R
+++ R/constructors.R
@@ -10,8 +10,9 @@
 #' @export
 cattonum_df <- function(x) {
   if (!missing(x)) {
-    if (!is.data.frame(x))
+    if (!is.data.frame(x)) {
       stop("`x` must be a data.frame or a tibble.", call. = FALSE)
+    }
     structure(x, class = c("cattonum_df", class(x)))
   } else {
     structure(data.frame(), class = c("cattonum_df", "data.frame"))
@@ -30,10 +31,24 @@ cattonum_df <- function(x) {
 #' cattonum_dfs()
 #' @export
 cattonum_dfs <- function(...) {
+  num_args <- nargs()
 
-  # could pass list of dfs
-  # individual dfs
-  # or nothing
+  if (!num_args) {
+    return(structure(list(), class = "cattonum_dfs"))
+  }
 
+  x <- list(...)
 
+  if (identical(num_args, 1L)) {
+    # TODO: could be a list holding one df???
+    if (!is.data.frame(x[[1L]])) {
+      stop("If passing a single argument to cattonum_dfs(), it must be a data.frame or a tibble.",
+        call. = FALSE
+      )
+    }
+    structure(x, class = "cattonum_dfs")
+  } else {
+    # TODO: check class of elements of x?
+    structure(x, class = "cattonum_dfs")
+  }
 }
--- tests/testthat/test-constructors.R
+++ tests/testthat/test-constructors.R
@@ -7,13 +7,34 @@ test_that("cattonum_df() works", {
 
   expect_error(cattonum_df(list()), regexp = "`x` must be a data.frame or a tibble.", fixed = TRUE)
 
-  expect_silent(tibble_arg <- cattonum_df(tibble::tibble(x = 1L)))
+  expect_silent(tibble_arg <- cattonum_df(tbl_fact))
   expect_s3_class(tibble_arg, c("cattonum_df", "tbl_df", "tbl", "data.frame"))
 
-  expect_silent(df_arg <- cattonum_df(data.frame(x = 1L)))
+  expect_silent(df_arg <- cattonum_df(df_fact))
   expect_s3_class(df_arg, c("cattonum_df", "data.frame"))
 })
 
 test_that("cattonum_dfs() works", {
+  expect_silent(missing_args <- cattonum_dfs())
+  expect_s3_class(missing_args, "cattonum_dfs")
 
+  expect_error(cattonum_dfs(1L), regexp = "If passing a single argument to cattonum_dfs(), it must be a data.frame or a tibble.", fixed = TRUE)
+
+  expect_silent(unnamed_tibble_arg <- cattonum_dfs(tbl_fact))
+  expect_s3_class(unnamed_tibble_arg, "cattonum_dfs")
+  expect_length(unnamed_tibble_arg, 1L)
+  expect_s3_class(unnamed_tibble_arg[[1]], c("tbl_df", "tbl", "data.frame"))
+
+  expect_silent(named_tibble_arg <- cattonum_dfs(foo = tbl_fact))
+  expect_s3_class(named_tibble_arg, "cattonum_dfs")
+  expect_length(named_tibble_arg, 1L)
+  expect_named(named_tibble_arg, "foo")
+  expect_s3_class(named_tibble_arg[[1]], c("tbl_df", "tbl", "data.frame"))
+
+  expect_silent(mixed_tibble_args <- cattonum_dfs(foo = tbl_fact, tbl_char))
+  expect_s3_class(mixed_tibble_args, "cattonum_dfs")
+  expect_length(mixed_tibble_args, 2L)
+  expect_named(mixed_tibble_args, c("foo", ""))
+  for (x in mixed_tibble_args)
+    expect_s3_class(x, c("tbl_df", "tbl", "data.frame"))
 })
