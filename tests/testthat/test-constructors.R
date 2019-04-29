095a4f3a9ad1969c16602ad6e97536c5ce93ef5a
Bernie Gray <bfgray3@gmail.com>
2019-04-28 22:05:15 -0400

    finish preliminary constructor tests; closes #33

--- tests/testthat/test-constructors.R
+++ tests/testthat/test-constructors.R
@@ -17,3 +17,25 @@ test_that("cattonum_df() works", {
   expect_equivalent(df_arg, df_fact)
 })
 
+test_that("cattonum_df2() works", {
+  expect_error(
+    cattonum_df2(NULL, iris),
+    regexp = "`test` cannot be non-NULL if `train` is NULL.",
+    fixed = TRUE
+  )
+
+  expect_silent(
+    tibble_args <- cattonum_df2(
+      tbl_fact[seq(3),],
+      tbl_fact[seq(3, nrow(tbl_fact)),]
+    )
+  )
+  expect_s3_class(tibble_args, "cattonum_df2")
+  expect_equivalent(
+    tibble_args,
+    list(
+      tbl_fact[seq(3),],
+      tbl_fact[seq(3, nrow(tbl_fact)),]
+    )
+  )
+})
