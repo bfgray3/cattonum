8d2c5165bd473d311b77a0daf446ade02ae82895
Bernie Gray <bfgray3@gmail.com>
2019-11-11 12:17:30 -0500

    summary cattonum methods; closes #38

--- R/cattonum-methods.R
+++ R/cattonum-methods.R
@@ -1,3 +1,8 @@
+has_data <- function(.x, .data_set) {
+  any(dim(.x[[.data_set]]))
+}
+
+
 #' @export
 print.cattonum_df <- function(x, ...) {
   cat("# A cattonum_df with the following data:\n")
@@ -7,27 +12,38 @@ print.cattonum_df <- function(x, ...) {
 
 #' @export
 print.cattonum_df2 <- function(x, ...) {
-  if (!any(dim(x[["train"]]))) {
+  if (!has_data(x, "train")) {
     cat("# A cattonum_df2 with no 'train' or 'test' data")
-  }
+  } else {
+    cat("# A cattonum_df2 with the following data:\n'train':\n")
+    print(x[["train"]])
 
-  cat("# A cattonum_df2 with the following data:\n'train':\n")
-  print(x[["train"]])
-
-  if (any(dim(x[["test"]]))) {
-    cat("'test':\n")
-    print(x[["test"]])
+    if (has_data(x, "test")) {
+      cat("'test':\n")
+      print(x[["test"]])
+    }
   }
+  invisible(x)
 }
 
 
 #' @export
 summary.cattonum_df <- function(object, ...) {
-
+  NextMethod()
 }
 
 
 #' @export
 summary.cattonum_df2 <- function(object, ...) {
+  if (!has_data(object, "train")) {
+    return()
+  } else {
+    train_summary <- summary(object[["train"]])
 
+    if (has_data(object, "test")) {
+      test_summary <- summary(object[["test"]])
+      return(list(train = train_summary, test = test_summary))
+    }
+  }
+  list(train = train_summary)
 }
